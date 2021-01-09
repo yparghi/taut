fb525770831baee93b6aeeb0b078360cf42a3b6d
Yash Parghi <yash@nytch.me>
2021-01-08 19:41:39 -0500

    Stub initial layout and build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -u
+set -e
+set -x
+
+mkdir -p build
+
+DIR="build/$1"
+rm -rf $DIR
+mkdir $DIR
+
+cp index.html "$DIR/index.html"
+cp taut.js "$DIR/taut.js"
+
+TAUT_VAR_SLACK_API_TOKEN="$(cat slack_api_token.txt)"
+sed -i 's/TAUT_VAR_SLACK_API_TOKEN/'"$TAUT_VAR_SLACK_API_TOKEN"'/g' "$DIR/taut.js"
