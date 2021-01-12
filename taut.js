7fa57d06a263c22b361e8a0725582579c8ecd7ec
Yash Parghi <yash@daybydaycoding.com>
2021-01-12 18:51:02 -0500

    Some basic parsing of channels in question

--- taut.js
+++ taut.js
@@ -1,19 +1,34 @@
 var SLACK_API_TOKEN = "TAUT_VAR_SLACK_API_TOKEN";
 
-const formData = new FormData();
-formData.append("token", SLACK_API_TOKEN);
+let channelsToRead = [ "eng", "general" ];
 
 let body = new URLSearchParams({
     "token": SLACK_API_TOKEN,
 });
 
-fetch("https://slack.com/api/conversations.list", {
-    method: "POST",
-    body: body,
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    }
-}).then(response => {
-    console.log(response.json());
-});
+function tautStart() {
+    fetch("https://slack.com/api/conversations.list", {
+        method: "POST",
+        body: body,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        }
+    }).then(response => {
+        if (response.ok) {
+            response.json().then(jsonBody => fetchMessages(jsonBody));
+        } else {
+            console.log("Response error!: " + response);
+        }
+    });
+};
+
+
+function fetchMessages(conversationsJson) {
+    console.log(conversationsJson);
+    conversationsJson.channels.forEach((channelObj) => {
+        if (channelsToRead.includes(channelObj.name)) {
+            console.log(channelObj);
+        }
+    });
+};
 
