0ad31a443b563d6074cd14d7e4618fc1e7476428
Yash Parghi <yash@daybydaycoding.com>
2021-01-12 17:27:41 -0500

    Macify build script with special case

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Options:
+# $1 Name for this dir/workspace/instance (arbitrary)
+# $2 API Key for the workspace
+
 set -u
 set -e
 set -x
@@ -14,4 +18,11 @@ cp index.html "$DIR/index.html"
 cp taut.js "$DIR/taut.js"
 
 TAUT_VAR_SLACK_API_TOKEN="$2"
-sed -i 's/TAUT_VAR_SLACK_API_TOKEN/'"$TAUT_VAR_SLACK_API_TOKEN"'/g' "$DIR/taut.js"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac parameter for a backup file, ugh.
+    sed -i '' 's/TAUT_VAR_SLACK_API_TOKEN/'"$TAUT_VAR_SLACK_API_TOKEN"'/g' "$DIR/taut.js"
+else
+    sed -i 's/TAUT_VAR_SLACK_API_TOKEN/'"$TAUT_VAR_SLACK_API_TOKEN"'/g' "$DIR/taut.js"
+fi
+
