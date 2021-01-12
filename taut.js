5636cef4d01d7bde77dad0da10d9f7102292dc62
Yash Parghi <yash@daybydaycoding.com>
2021-01-12 17:55:55 -0500

    Finally get the fetch right for the slack api

--- taut.js
+++ taut.js
@@ -1,2 +1,19 @@
 var SLACK_API_TOKEN = "TAUT_VAR_SLACK_API_TOKEN";
 
+const formData = new FormData();
+formData.append("token", SLACK_API_TOKEN);
+
+let body = new URLSearchParams({
+    "token": SLACK_API_TOKEN,
+});
+
+fetch("https://slack.com/api/conversations.list", {
+    method: "POST",
+    body: body,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    }
+}).then(response => {
+    console.log(response.json());
+});
+
