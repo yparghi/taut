74063456dfc4b6b81c69f9e4eb322270465d08b8
Yash Parghi <yash@daybydaycoding.com>
2021-01-12 19:28:17 -0500

    Start retrieving messages

--- taut.js
+++ taut.js
@@ -2,12 +2,22 @@ var SLACK_API_TOKEN = "TAUT_VAR_SLACK_API_TOKEN";
 
 let channelsToRead = [ "eng", "general" ];
 
-let body = new URLSearchParams({
-    "token": SLACK_API_TOKEN,
-});
 
 function tautStart() {
-    fetch("https://slack.com/api/conversations.list", {
+    doFetch(
+        "https://slack.com/api/conversations.list",
+        {},
+        fetchMessages);
+};
+
+
+function doFetch(urlStr, paramsDict, callbackFun) {
+    let body = new URLSearchParams({
+        "token": SLACK_API_TOKEN,
+        ...paramsDict,
+    });
+
+    fetch(urlStr, {
         method: "POST",
         body: body,
         headers: {
@@ -15,7 +25,7 @@ function tautStart() {
         }
     }).then(response => {
         if (response.ok) {
-            response.json().then(jsonBody => fetchMessages(jsonBody));
+            response.json().then(jsonBody => callbackFun(jsonBody));
         } else {
             console.log("Response error!: " + response);
         }
@@ -23,12 +33,45 @@ function tautStart() {
 };
 
 
+// Thanks to: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Cyclic_object_value#Examples
+const makeCircularReplacer = () => {
+    const seen = new WeakSet();
+    return (key, value) => {
+        if (typeof value === "object" && value !== null) {
+            if (seen.has(value)) {
+                if (value.hasOwnProperty("shortName")) {
+                    return "[Circular: " + value.shortName + "]";
+                } else {
+                    return "[Circular]";
+                }
+            }
+            seen.add(value);
+        }
+        return value;
+    };
+};
+
+function objToString(obj) {
+    return JSON.stringify(obj, makeCircularReplacer(), 4);
+}
+
+
 function fetchMessages(conversationsJson) {
-    console.log(conversationsJson);
     conversationsJson.channels.forEach((channelObj) => {
         if (channelsToRead.includes(channelObj.name)) {
-            console.log(channelObj);
+            doFetch(
+                "https://slack.com/api/conversations.history",
+                {
+                    "channel": channelObj.id,
+                    "limit": 20,
+                },
+                displayMessages);
         }
     });
 };
+
+
+function displayMessages(messagesJson) {
+    console.log("Got messages: " + objToString(messagesJson));
+}
 
