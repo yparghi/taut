d9a3b7d0fbe5d99d223db3a741fe19deac4dfc7f
Yash Parghi <yash@daybydaycoding.com>
2021-01-12 22:21:02 -0500

    Templating for the html title

--- build.sh
+++ build.sh
@@ -17,12 +17,16 @@ mkdir $DIR
 cp index.html "$DIR/index.html"
 cp taut.js "$DIR/taut.js"
 
+TAUT_VAR_INSTANCE_NAME="$1"
 TAUT_VAR_SLACK_API_TOKEN="$2"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    # Mac parameter for a backup file, ugh.
+    # Handle mac sed, ugh.
     sed -i '' 's/TAUT_VAR_SLACK_API_TOKEN/'"$TAUT_VAR_SLACK_API_TOKEN"'/g' "$DIR/taut.js"
+    sed -i '' 's/TAUT_VAR_INSTANCE_NAME/'"$TAUT_VAR_INSTANCE_NAME"'/g' "$DIR/index.html"
+
 else
     sed -i 's/TAUT_VAR_SLACK_API_TOKEN/'"$TAUT_VAR_SLACK_API_TOKEN"'/g' "$DIR/taut.js"
+    sed -i 's/TAUT_VAR_INSTANCE_NAME/'"$TAUT_VAR_INSTANCE_NAME"'/g' "$DIR/index.html"
 fi
 
