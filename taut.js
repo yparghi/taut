4e3a0bf2b2281394f47226c1778fbb2e4ef4ba6e
Yash Parghi <yash@daybydaycoding.com>
2021-01-29 22:12:26 -0500

    TODO for faster debug evaluation

--- taut.js
+++ taut.js
@@ -114,6 +114,8 @@ function debug(message) {
 
 
 function fetchMessages(conversationsJson) {
+    // TODO: Consider switching debug to use thunks, for lazy evaluation in
+    // case debug is disabled.
     debug("Got conversations:\n" + objToString(conversationsJson));
 
     conversationsJson.channels.forEach((conversationObj) => {
